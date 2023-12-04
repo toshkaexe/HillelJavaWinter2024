063996659bcfa61bbf9e2471d0a67bc18b807d54
Anton <anton.zeltser@gmail.com>
2023-12-04 21:19:49 +0100

    add  template classes to the package homework2

--- src/main/java/homework2/Average.java
+++ src/main/java/homework2/Average.java
@@ -1,5 +1,6 @@
 package homework2;
 
+//1. Програма, яка знаходить середнє арифметичне значення двох чисел.
 public class Average{
     public static void main(String[] args) {
         System.out.println("Hello World");
