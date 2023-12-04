2160378f382267e20d551777b84dd7ee2493df8c
Anton <anton.zeltser@gmail.com>
2023-12-04 21:27:37 +0100

    added taks 1

--- src/main/java/homework2/Average.java
+++ src/main/java/homework2/Average.java
@@ -2,7 +2,17 @@ package homework2;
 
 //1. Програма, яка знаходить середнє арифметичне значення двох чисел.
 public class Average {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        int a = 11;
+        int b = 2;
+
+        double average = (double) (a + b) / 2;
+
+        System.out.println(String.format("a = %d ", a));
+        System.out.println(String.format("b = %d ", b));
+        System.out.println(String.format("average of %d and %d is %f", a, b, average));
     }
 }
