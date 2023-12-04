0415de7f7d46b554ea81fd6eb9779ebe91965558
Anton <anton.zeltser@gmail.com>
2023-12-04 21:30:35 +0100

    added task 2

--- src/main/java/homework2/AverageN.java
+++ src/main/java/homework2/AverageN.java
@@ -1,10 +1,41 @@
 package homework2;
 
+import java.util.Scanner;
+
 /*
 2. Програма, яка знаходить середнє арифметичне значення довільної кількості чисел.
  */
 public class AverageN {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введіть числа для обчислення середнього " +
+                "(введіть 0, щоб завершити введення):");
+
+        double sum = 0;
+        int count = 0;
+
+        while (true) {
+            double number = scanner.nextDouble();
+
+            if (number == 0) {
+                break;
+            }
+
+            sum += number;
+            count++;
+        }
+
+        if (count > 0) {
+            double average = sum / count;
+            System.out.println("Середнє арифметичне: " + average);
+        } else {
+            System.out.println("Не введено жодного числа.");
+        }
+
+        scanner.close();
     }
+
+
 }
