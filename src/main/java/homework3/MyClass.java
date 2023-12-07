9a887ae18e6bb96d5fad3be04d48e704cee3018e
Anton <anton.zeltser@gmail.com>
2023-12-07 18:54:56 +0100

    homework 3 task 1

--- src/main/java/homework3/MyClass.java
+++ src/main/java/homework3/MyClass.java
@@ -17,5 +17,11 @@ public class MyClass {
     public static void main(String[] args) {
 
 
+        for(int i=1; i<=99;i = i+2)
+        {
+            System.out.println(i);
+        }
+
+
     }
 }
