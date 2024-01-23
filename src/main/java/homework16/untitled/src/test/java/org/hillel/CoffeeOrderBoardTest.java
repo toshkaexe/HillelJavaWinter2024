e4ea80636f58503ca29e1da242925d0d33c1ba50
Anton <anton.zeltser@gmail.com>
2024-01-23 20:58:40 +0100

    added unit-tests

--- src/main/java/homework16/untitled/src/test/java/org/hillel/CoffeeOrderBoardTest.java
+++ src/main/java/homework16/untitled/src/test/java/org/hillel/CoffeeOrderBoardTest.java
@@ -21,36 +21,37 @@ public class CoffeeOrderBoardTest {
         coffeeOrderBoard.add(order1);
         coffeeOrderBoard.add(order2);
 
-        String expectedOutput = "Oder 1 Alen\nOder 2 Yoda\n";
+        String expectedOutput = "Order 1 for Alen\nOrder 2 for Yoda\n";
         assertEquals(expectedOutput, coffeeOrderBoard.printOrders());
     }
 
     @Test
     public void testDeliver() {
-        Order order1 = new Order("Alen");
-        Order order2 = new Order("Yoda");
-
-        coffeeOrderBoard.add(order1);
-        coffeeOrderBoard.add(order2);
-
-        coffeeOrderBoard.deliver();
-
-        String expectedOutput = "Order 2 for Yoda\n";
-        assertEquals(expectedOutput, coffeeOrderBoard.printOrders());
-    }
-
-    @Test
-    public void testDeliverByNumber() {
-        Order order3 = new Order("Anna");
-        Order order4 = new Order("Pavel");
+        Order order3 = new Order("Max");
+        Order order4 = new Order("Lukas");
 
         coffeeOrderBoard.add(order3);
         coffeeOrderBoard.add(order4);
 
+        coffeeOrderBoard.deliver();
+
+        String expectedOutput = "Order 2 for Lukas\n";
+        assertEquals(expectedOutput, coffeeOrderBoard.printOrders());
+    }
+
+
+    @Test
+    public void testDeliverByNumber() {
+        Order order5 = new Order("Anna");
+        Order order6 = new Order("Pavel");
+
+        coffeeOrderBoard.add(order5);
+        coffeeOrderBoard.add(order6);
+
         coffeeOrderBoard.deliver(3);
 
-        String expectedOutput = "Order 1 for Anna\n" +
-                                "Order 2 for Pavel\n";
+        String expectedOutput =
+                "Order 4 for Pavel\n";
         assertEquals(expectedOutput, coffeeOrderBoard.printOrders());
     }
 
