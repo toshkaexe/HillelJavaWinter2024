26548714848dc01ea7e028df1057407a640bdab9
Anton <anton.zeltser@gmail.com>
2024-03-04 21:24:17 +0100

    added hw24

--- src/main/java/homework24/untitled/src/main/java/org/springframework/Order.java
+++ src/main/java/homework24/untitled/src/main/java/org/springframework/Order.java
@@ -1,62 +1,20 @@
 package org.springframework;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
-
+@Getter
+@Setter
+@AllArgsConstructor
 public class Order {
     private Integer id;
     private Date date;
     private double cost;
     private List<Product> products;
 
-    public Order(Integer id, Date date, double cost, List<Product> products) {
-        this.id = id;
-        this.date = date;
-        this.cost = cost;
-        this.products = products;
-    }
 
-    public int getId() {
-        return id;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public double getCost() {
-        return cost;
-    }
-
-    public List<Product> getProducts() {
-        return products;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public void setCost(double cost) {
-        this.cost = cost;
-    }
-
-    public void setProducts(List<Product> products) {
-        this.products = products;
-    }
-
-    @Override
-    public String toString() {
-        return "Order{" +
-                "id=" + id +
-                ", date=" + date +
-                ", cost=" + cost +
-                ", products=" + products +
-                '}';
-    }
 }
